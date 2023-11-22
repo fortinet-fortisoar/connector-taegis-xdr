1c4a08b72931787d9e3acfb780842216fdaf14c9
cs-adarsh-gupta <gadarsh@fortinet.com>
2023-11-22 11:39:37 +0530

    Updated description as per doc

--- taegis-xdr/operations.py
+++ taegis-xdr/operations.py
@@ -1,8 +1,9 @@
-""" Copyright start
-Copyright (C) 2008 - 2023 Fortinet Inc.
-All rights reserved.
-FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
-Copyright end """
+"""
+Copyright start
+MIT License
+Copyright (c) 2023 Fortinet Inc
+Copyright end
+"""
 import requests
 import time
 from connectors.core.connector import get_logger, ConnectorError
--- taegis-xdr/taegis_xdr_api_auth.py
+++ taegis-xdr/taegis_xdr_api_auth.py
@@ -1,8 +1,9 @@
-""" Copyright start
-  Copyright (C) 2008 - 2023 Fortinet Inc.
-  All rights reserved.
-  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
-  Copyright end """
+"""
+Copyright start
+MIT License
+Copyright (c) 2023 Fortinet Inc
+Copyright end
+"""
 
 from requests import request
 from time import time, ctime
